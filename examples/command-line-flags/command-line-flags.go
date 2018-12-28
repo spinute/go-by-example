5a2913c487facd19d5ec3db3defb74d2456fa8ab
spinute <spinutids@gmail.com>
2018-12-29 05:17:36 +0800

    translate command-line-flags

--- examples/command-line-flags/command-line-flags.go
+++ examples/command-line-flags/command-line-flags.go
@@ -1,49 +1,39 @@
-// [_Command-line flags_](http://en.wikipedia.org/wiki/Command-line_interface#Command-line_option)
-// are a common way to specify options for command-line
-// programs. For example, in `wc -l` the `-l` is a
-// command-line flag.
+// [_コマンドラインフラグ_](http://en.wikipedia.org/wiki/Command-line_interface#Command-line_option)
+// はコマンドラインからオプションを指定するためによく使われる。
+// 例えば `wc -l` の `-l` はコマンドラインフラグである。
 
 package main
 
-// Go provides a `flag` package supporting basic
-// command-line flag parsing. We'll use this package to
-// implement our example command-line program.
+// Go の `flag` パッケージを使ってコマンドラインフラグをパースできる。
+// 実際にコマンドラインプログラムを作って、このパッケージを使ってみよう。
 import "flag"
 import "fmt"
 
 func main() {
 
-    // Basic flag declarations are available for string,
-    // integer, and boolean options. Here we declare a
-    // string flag `word` with a default value `"foo"`
-    // and a short description. This `flag.String` function
-    // returns a string pointer (not a string value);
-    // we'll see how to use this pointer below.
-    wordPtr := flag.String("word", "foo", "a string")
+	// 文字列、整数、真偽値のオプションを受け取るフラグを宣言できる。
+	// ここではフラグ `word` を宣言し、そのデフォルト値を `"foo"` とし、フラグの短い説明を与えている。
+	// 関数 `flag.String` は（文字列そのものではなく）文字列のポインタを返す。
+	// このポインタの使い方は追って説明する。
+	wordPtr := flag.String("word", "foo", "a string")
 
-    // This declares `numb` and `fork` flags, using a
-    // similar approach to the `word` flag.
-    numbPtr := flag.Int("numb", 42, "an int")
-    boolPtr := flag.Bool("fork", false, "a bool")
+	// `word` と同様のやり方で、フラグ `numb`、`fork` を宣言している。
+	numbPtr := flag.Int("numb", 42, "an int")
+	boolPtr := flag.Bool("fork", false, "a bool")
 
-    // It's also possible to declare an option that uses an
-    // existing var declared elsewhere in the program.
-    // Note that we need to pass in a pointer to the flag
-    // declaration function.
-    var svar string
-    flag.StringVar(&svar, "svar", "bar", "a string var")
+	// プログラム内の別の場所で宣言した変数を使い、オプションを宣言することもできる。
+	// なお、この場合変数のポインタを渡すことに注意する。
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
 
-    // Once all flags are declared, call `flag.Parse()`
-    // to execute the command-line parsing.
-    flag.Parse()
+	// すべてのフラグを宣言したら、`flag.Parse()` を呼んでコマンドラインをパースする。
+	flag.Parse()
 
-    // Here we'll just dump out the parsed options and
-    // any trailing positional arguments. Note that we
-    // need to dereference the pointers with e.g. `*wordPtr`
-    // to get the actual option values.
-    fmt.Println("word:", *wordPtr)
-    fmt.Println("numb:", *numbPtr)
-    fmt.Println("fork:", *boolPtr)
-    fmt.Println("svar:", svar)
-    fmt.Println("tail:", flag.Args())
+	// パースしたオプションと、余りの引数を表示する。
+	// `*wordPtr` のように参照を剥がしてオプション値を呼んでいることに注意する。
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *boolPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
 }
