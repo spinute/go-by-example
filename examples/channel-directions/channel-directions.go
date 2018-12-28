b1530e2172c8fc005c68cc66f2c1c6d54de31d78
spinute <spinutids@gmail.com>
2018-12-28 23:18:58 +0900

    translate channel-directions

--- examples/channel-directions/channel-directions.go
+++ examples/channel-directions/channel-directions.go
@@ -1,30 +1,26 @@
-// When using channels as function parameters, you can
-// specify if a channel is meant to only send or receive
-// values. This specificity increases the type-safety of
-// the program.
+// チャネルを関数に渡すとき、そのチャネルが送信専用・受信専用であることを明記できる。
+// こうするとプログラムの型安全性が増す。
 
 package main
 
 import "fmt"
 
-// This `ping` function only accepts a channel for sending
-// values. It would be a compile-time error to try to
-// receive on this channel.
+// この関数 `ping` の引数であるチャネル `pings` は送信専用である。
+// このチャネルから受信しようとすれば、コンパイルエラーになる。
 func ping(pings chan<- string, msg string) {
-    pings <- msg
+	pings <- msg
 }
 
-// The `pong` function accepts one channel for receives
-// (`pings`) and a second for sends (`pongs`).
+// この関数 `pong` の引数であるチャネル `pings` は受信専用、`pongs` は送信専用である。`
 func pong(pings <-chan string, pongs chan<- string) {
-    msg := <-pings
-    pongs <- msg
+	msg := <-pings
+	pongs <- msg
 }
 
 func main() {
-    pings := make(chan string, 1)
-    pongs := make(chan string, 1)
-    ping(pings, "passed message")
-    pong(pings, pongs)
-    fmt.Println(<-pongs)
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
 }
