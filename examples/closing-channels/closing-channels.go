e91820847f83274d0de2e5013a8e1d62beda96ef
spinute <spinutids@gmail.com>
2018-12-28 23:57:24 +0900

    translate closing-channels

--- examples/closing-channels/closing-channels.go
+++ examples/closing-channels/closing-channels.go
@@ -1,50 +1,42 @@
-// _Closing_ a channel indicates that no more values
-// will be sent on it. This can be useful to communicate
-// completion to the channel's receivers.
+// チャネルを_閉じる_とそのチャネルにはもう値を送らないことを示せる。
+// これはチャネルの受信者に終わりを伝えるのに使える。
 
 package main
 
 import "fmt"
 
-// In this example we'll use a `jobs` channel to
-// communicate work to be done from the `main()` goroutine
-// to a worker goroutine. When we have no more jobs for
-// the worker we'll `close` the `jobs` channel.
+// この例ではチャネル `jobs` を使って `main()` ゴルーチンからワーカーゴルーチンに仕事を送る。
+// ワーカーに実行してもらう仕事を送り終えると、チャネル `jobs` を閉じる。
 func main() {
-    jobs := make(chan int, 5)
-    done := make(chan bool)
+	jobs := make(chan int, 5)
+	done := make(chan bool)
 
-    // Here's the worker goroutine. It repeatedly receives
-    // from `jobs` with `j, more := <-jobs`. In this
-    // special 2-value form of receive, the `more` value
-    // will be `false` if `jobs` has been `close`d and all
-    // values in the channel have already been received.
-    // We use this to notify on `done` when we've worked
-    // all our jobs.
-    go func() {
-        for {
-            j, more := <-jobs
-            if more {
-                fmt.Println("received job", j)
-            } else {
-                fmt.Println("received all jobs")
-                done <- true
-                return
-            }
-        }
-    }()
+	// ワーカーを起動する。
+	// このゴルーチンは `j, more := <-jobs` の行で繰り返し `jobs` から値を受信する。
+	// チャネルから2つ値を読み出しているのがキモだ。
+	// チャネルが閉じられており、そのチャネルの値をすべて受信し終えているとき、`more` は false になる。
+	// こうして最後の仕事を終えたことがわかるので、`done` を送り返せる。
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received job", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
 
-    // This sends 3 jobs to the worker over the `jobs`
-    // channel, then closes it.
-    for j := 1; j <= 3; j++ {
-        jobs <- j
-        fmt.Println("sent job", j)
-    }
-    close(jobs)
-    fmt.Println("sent all jobs")
+	// チャネル `jobs` を通じて3つの仕事をワーカーに送り、その後チャネルを閉じる。
+	for j := 1; j <= 3; j++ {
+		jobs <- j
+		fmt.Println("sent job", j)
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
 
-    // We await the worker using the
-    // [synchronization](channel-synchronization) approach
-    // we saw earlier.
-    <-done
+	// 既に紹介した[同期](channel-synchronization)のやり方で、ワーカーを待つ。
+	<-done
 }
