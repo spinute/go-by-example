301d25870702b2d616fa4146a7228c24ac1ce5af
spinute <spinutids@gmail.com>
2018-12-28 22:06:24 +0900

    translate arrays.go

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -1,5 +1,4 @@
-// In Go, an _array_ is a numbered sequence of elements of a
-// specific length.
+// Go 言語における _array_ （配列）とは、決まった数だけ要素を並べた列である。
 
 package main
 
@@ -7,36 +6,32 @@ import "fmt"
 
 func main() {
 
-    // Here we create an array `a` that will hold exactly
-    // 5 `int`s. The type of elements and length are both
-    // part of the array's type. By default an array is
-    // zero-valued, which for `int`s means `0`s.
-    var a [5]int
-    fmt.Println("emp:", a)
+	// `a` は int 型の値5個からなる配列。
+	// 配列の型を見ると要素の型と個数がわかる。
+	// 配列を宣言すると、要素はゼロ値（int 型の場合は0）になる。
+	var a [5]int
+	fmt.Println("emp:", a)
 
-    // We can set a value at an index using the
-    // `array[index] = value` syntax, and get a value with
-    // `array[index]`.
-    a[4] = 100
-    fmt.Println("set:", a)
-    fmt.Println("get:", a[4])
+	// 配列の index 番目の値を value にするには `array[index] = value` と書く。
+	// index 番目の値を読み出すには `array[index]` と書く。
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
 
-    // The builtin `len` returns the length of an array.
-    fmt.Println("len:", len(a))
+	// 組み込み関数 `len` は配列の長さを返す。
+	fmt.Println("len:", len(a))
 
-    // Use this syntax to declare and initialize an array
-    // in one line.
-    b := [5]int{1, 2, 3, 4, 5}
-    fmt.Println("dcl:", b)
+	// このように書けば、配列の宣言と初期化を一行で済ませられる。
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
 
-    // Array types are one-dimensional, but you can
-    // compose types to build multi-dimensional data
-    // structures.
-    var twoD [2][3]int
-    for i := 0; i < 2; i++ {
-        for j := 0; j < 3; j++ {
-            twoD[i][j] = i + j
-        }
-    }
-    fmt.Println("2d: ", twoD)
+	// 配列型は一次元である。
+	// しかし、配列型を組み合わせて多次元のデータ構造を作ることもできる。
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
 }
