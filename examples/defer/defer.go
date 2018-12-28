e1956f5836f75651e1679ebf6a7e8b480b7bd722
spinute <spinutids@gmail.com>
2018-12-29 05:30:46 +0800

    translato defer

--- examples/defer/defer.go
+++ examples/defer/defer.go
@@ -1,44 +1,39 @@
-// _Defer_ is used to ensure that a function call is
-// performed later in a program's execution, usually for
-// purposes of cleanup. `defer` is often used where e.g.
-// `ensure` and `finally` would be used in other languages.
+// _Defer_ を使って、ある関数を確実に後で呼ばせることができる。
+// これは後片付けにしばしば使われる機能だ。
+// 他の言語だと `ensure` や `finally` といったキーワードを使う場面で、Go では `defer` を使う。
 
 package main
 
 import "fmt"
 import "os"
 
-// Suppose we wanted to create a file, write to it,
-// and then close when we're done. Here's how we could
-// do that with `defer`.
+// ファイルを作り、書き込み、最後にファイルを閉じたいとする。
+// これを `defer` を使ってやってみよう。
 func main() {
 
-    // Immediately after getting a file object with
-    // `createFile`, we defer the closing of that file
-    // with `closeFile`. This will be executed at the end
-    // of the enclosing function (`main`), after
-    // `writeFile` has finished.
-    f := createFile("/tmp/defer.txt")
-    defer closeFile(f)
-    writeFile(f)
+	// `createFile` でファイルオブジェクトを得た直後に、ファイルを閉じるよう `closeFile` を `defer` しておく。
+	// 直近の関数（`main`）の最後、つまり `writeFile` が終わるときに、defer した関数が実行される。
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
 }
 
 func createFile(p string) *os.File {
-    fmt.Println("creating")
-    f, err := os.Create(p)
-    if err != nil {
-        panic(err)
-    }
-    return f
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
 
 func writeFile(f *os.File) {
-    fmt.Println("writing")
-    fmt.Fprintln(f, "data")
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
 
 }
 
 func closeFile(f *os.File) {
-    fmt.Println("closing")
-    f.Close()
+	fmt.Println("closing")
+	f.Close()
 }
