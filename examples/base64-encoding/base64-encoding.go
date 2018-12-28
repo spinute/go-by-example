3b4031d9b7a862ba3b34ae5d6a05eff2a0d48ec0
spinute <spinutids@gmail.com>
2018-12-28 23:00:28 +0900

    translate bsae64-encoding

--- examples/base64-encoding/base64-encoding.go
+++ examples/base64-encoding/base64-encoding.go
@@ -1,37 +1,33 @@
-// Go provides built-in support for [base64
-// encoding/decoding](http://en.wikipedia.org/wiki/Base64).
+// Go は組み込みで [base64](http://en.wikipedia.org/wiki/Base64) という
+// エンコード形式をサポートしている。
 
 package main
 
-// This syntax imports the `encoding/base64` package with
-// the `b64` name instead of the default `base64`. It'll
-// save us some space below.
+// このように書くと、`encoding/base64` パッケージを（デフォルトの base64 の代わりに）`b64` という名前でインポートする。
+// これはスペースを節約するのに役立つ。
 import b64 "encoding/base64"
 import "fmt"
 
 func main() {
 
-    // Here's the `string` we'll encode/decode.
-    data := "abc123!?$*&()'-=@~"
+	// 今からエンコード・デコードする文字列
+	data := "abc123!?$*&()'-=@~"
 
-    // Go supports both standard and URL-compatible
-    // base64. Here's how to encode using the standard
-    // encoder. The encoder requires a `[]byte` so we
-    // cast our `string` to that type.
-    sEnc := b64.StdEncoding.EncodeToString([]byte(data))
-    fmt.Println(sEnc)
+	// Go は標準の base64 と、URL 文字列として使える base64 をいずれもサポートしている。
+	// ここでは標準の方を使う。
+	// エンコーダには `[]byte` 型の値を渡すので、ここでは `string` 型からキャストしている。
+	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
 
-    // Decoding may return an error, which you can check
-    // if you don't already know the input to be
-    // well-formed.
-    sDec, _ := b64.StdEncoding.DecodeString(sEnc)
-    fmt.Println(string(sDec))
-    fmt.Println()
+	// 入力によってはデコードは失敗するかもしれない。
+	// その可能性があるなら、返り値からエラーの有無を確認できる。
+	sDec, _ := b64.StdEncoding.DecodeString(sEnc)
+	fmt.Println(string(sDec))
+	fmt.Println()
 
-    // This encodes/decodes using a URL-compatible base64
-    // format.
-    uEnc := b64.URLEncoding.EncodeToString([]byte(data))
-    fmt.Println(uEnc)
-    uDec, _ := b64.URLEncoding.DecodeString(uEnc)
-    fmt.Println(string(uDec))
+	// 根では URL として使える base64 フォーマットを使ってみる。
+	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
+	fmt.Println(uEnc)
+	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
+	fmt.Println(string(uDec))
 }
