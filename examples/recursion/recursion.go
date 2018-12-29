1956f8bdda24f751d4b31f323b4ec26f69bc3edf
spinute <spinutids@gmail.com>
2018-12-29 15:04:08 +0800

    translace recursion

--- examples/recursion/recursion.go
+++ examples/recursion/recursion.go
@@ -1,20 +1,18 @@
-// Go supports
-// <a href="http://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>recursive functions</em></a>.
-// Here's a classic factorial example.
+// Go は<a href="http://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>再帰関数</em></a>をサポートしている。
+// この例は古典的な階乗を計算するものだ。
 
 package main
 
 import "fmt"
 
-// This `fact` function calls itself until it reaches the
-// base case of `fact(0)`.
+// この関数 `fact` は、ベースケースである `fact(0)` に到達するまで自身を繰り返し呼び出す。
 func fact(n int) int {
-    if n == 0 {
-        return 1
-    }
-    return n * fact(n-1)
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n-1)
 }
 
 func main() {
-    fmt.Println(fact(7))
+	fmt.Println(fact(7))
 }
