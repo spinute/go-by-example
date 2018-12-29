fd76aa969b61368d13edd6b45f0e5802c5d82453
spinute <spinutids@gmail.com>
2018-12-29 13:31:05 +0800

    translate number-parsing

--- examples/number-parsing/number-parsing.go
+++ examples/number-parsing/number-parsing.go
@@ -1,40 +1,36 @@
-// Parsing numbers from strings is a basic but common task
-// in many programs; here's how to do it in Go.
+// 文字列から数値へのパースは、多くのプログラムに現れるパターンである。
+// ここでは、Go でのやり方を紹介する。
 
 package main
 
-// The built-in package `strconv` provides the number
-// parsing.
+// `strconv` パッケージを使って数をパースできる。
 import "strconv"
 import "fmt"
 
 func main() {
 
-    // With `ParseFloat`, this `64` tells how many bits of
-    // precision to parse.
-    f, _ := strconv.ParseFloat("1.234", 64)
-    fmt.Println(f)
+	// `ParseFloat` の `64` は何ビットの精度でパースするかを指定する。
+	f, _ := strconv.ParseFloat("1.234", 64)
+	fmt.Println(f)
 
-    // For `ParseInt`, the `0` means infer the base from
-    // the string. `64` requires that the result fit in 64
-    // bits.
-    i, _ := strconv.ParseInt("123", 0, 64)
-    fmt.Println(i)
+	// `ParseInt` の `0` は基数を文字列から推測させることを表す。
+	// `64` は、結果が64ビットに収まることを要求する。
+	i, _ := strconv.ParseInt("123", 0, 64)
+	fmt.Println(i)
 
-    // `ParseInt` will recognize hex-formatted numbers.
-    d, _ := strconv.ParseInt("0x1c8", 0, 64)
-    fmt.Println(d)
+	// `ParseInt` は8進記数法で書かれた文字列をパースできる。
+	d, _ := strconv.ParseInt("0x1c8", 0, 64)
+	fmt.Println(d)
 
-    // A `ParseUint` is also available.
-    u, _ := strconv.ParseUint("789", 0, 64)
-    fmt.Println(u)
+	// `ParseUint` もある。
+	u, _ := strconv.ParseUint("789", 0, 64)
+	fmt.Println(u)
 
-    // `Atoi` is a convenience function for basic base-10
-    // `int` parsing.
-    k, _ := strconv.Atoi("135")
-    fmt.Println(k)
+	// `Atoi` は基数が10の `int` 型の値をパースするときに便利である。
+	k, _ := strconv.Atoi("135")
+	fmt.Println(k)
 
-    // Parse functions return an error on bad input.
-    _, e := strconv.Atoi("wat")
-    fmt.Println(e)
+	// パースする関数は、入力の形式が正しくないとエラーを返す。
+	_, e := strconv.Atoi("wat")
+	fmt.Println(e)
 }
