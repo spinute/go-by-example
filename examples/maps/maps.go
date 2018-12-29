1f75134b91059f312586a668610aa6fbcd3eab47
spinute <spinutids@gmail.com>
2018-12-29 12:47:15 +0800

    translate maps

--- examples/maps/maps.go
+++ examples/maps/maps.go
@@ -1,5 +1,5 @@
-// _Maps_ are Go's built-in [associative data type](http://en.wikipedia.org/wiki/Associative_array)
-// (sometimes called _hashes_ or _dicts_ in other languages).
+// _マップ_は Go に組み込みの[連想データ型](http://en.wikipedia.org/wiki/Associative_array)である。
+// これは他の言語では_ハッシュ_や_辞書_と呼ばれることがあるものだ。
 
 package main
 
@@ -7,44 +7,34 @@ import "fmt"
 
 func main() {
 
-    // To create an empty map, use the builtin `make`:
-    // `make(map[key-type]val-type)`.
-    m := make(map[string]int)
+	// 空のマップを作るには、組み込みの `make` を使って、`make(map[key-type]val-type)` のように書く。
+	m := make(map[string]int)
 
-    // Set key/value pairs using typical `name[key] = val`
-    // syntax.
-    m["k1"] = 7
-    m["k2"] = 13
+	// キーと値の組みを登録するには、`name[key] = val` のように書く。
+	m["k1"] = 7
+	m["k2"] = 13
 
-    // Printing a map with e.g. `fmt.Println` will show all of
-    // its key/value pairs.
-    fmt.Println("map:", m)
+	// マップを例えば `fmt.Println` などを使って表示すると、キーと値の組みをすべて表示する。
+	fmt.Println("map:", m)
 
-    // Get a value for a key with `name[key]`.
-    v1 := m["k1"]
-    fmt.Println("v1: ", v1)
+	// マップから値を読み出すには、`name[key]` のように書く。
+	v1 := m["k1"]
+	fmt.Println("v1: ", v1)
 
-    // The builtin `len` returns the number of key/value
-    // pairs when called on a map.
-    fmt.Println("len:", len(m))
+	// 組み込みの `len` は、マップに含まれるキーと値の組みの個数を返す。
+	fmt.Println("len:", len(m))
 
-    // The builtin `delete` removes key/value pairs from
-    // a map.
-    delete(m, "k2")
-    fmt.Println("map:", m)
+	// 組み込みの `delete` はマップからキーと値の組みを削除する。
+	delete(m, "k2")
+	fmt.Println("map:", m)
 
-    // The optional second return value when getting a
-    // value from a map indicates if the key was present
-    // in the map. This can be used to disambiguate
-    // between missing keys and keys with zero values
-    // like `0` or `""`. Here we didn't need the value
-    // itself, so we ignored it with the _blank identifier_
-    // `_`.
-    _, prs := m["k2"]
-    fmt.Println("prs:", prs)
+	// オプションの2つ目の返り値は、マップにキーが含まれるかどうかを表す真偽値である。
+	// これはキーが入っていない場合と、そのキーの値としてゼロ値（`0` や `""` など）が入っている場合とを区別するために使える。
+	// ここでは、値はいらないので、_ブランク識別子_ `_` を使って、無視している。
+	_, prs := m["k2"]
+	fmt.Println("prs:", prs)
 
-    // You can also declare and initialize a new map in
-    // the same line with this syntax.
-    n := map[string]int{"foo": 1, "bar": 2}
-    fmt.Println("map:", n)
+	// 以下のように書くと、マップの宣言と初期化を同じ行で済ませられる。
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
 }
