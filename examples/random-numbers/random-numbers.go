81d32d1e57b2fac5f1413b06facfef3c5d47178c
spinute <spinutids@gmail.com>
2018-12-29 14:04:44 +0800

    translate random-numbers

--- examples/random-numbers/random-numbers.go
+++ examples/random-numbers/random-numbers.go
@@ -1,6 +1,4 @@
-// Go's `math/rand` package provides
-// [pseudorandom number](http://en.wikipedia.org/wiki/Pseudorandom_number_generator)
-// generation.
+// Go の `math/rand` パッケージは[疑似乱数](http://en.wikipedia.org/wiki/Pseudorandom_number_generator)生成器である。
 
 package main
 
@@ -10,45 +8,40 @@ import "math/rand"
 
 func main() {
 
-    // For example, `rand.Intn` returns a random `int` n,
-    // `0 <= n < 100`.
-    fmt.Print(rand.Intn(100), ",")
-    fmt.Print(rand.Intn(100))
-    fmt.Println()
+	// 例えば、`rand.Intn` は `int` 型の値 n であって、`0 <= n < 100` を満たすものをランダムに選んで返す。
+	fmt.Print(rand.Intn(100), ",")
+	fmt.Print(rand.Intn(100))
+	fmt.Println()
 
-    // `rand.Float64` returns a `float64` `f`,
-    // `0.0 <= f < 1.0`.
-    fmt.Println(rand.Float64())
+	// `rand.Float64` は `float64` 型の値 `f` であって、`0.0 <= f < 1.0` を満たすものをランダムに選んで返す。
+	fmt.Println(rand.Float64())
 
-    // This can be used to generate random floats in
-    // other ranges, for example `5.0 <= f' < 10.0`.
-    fmt.Print((rand.Float64()*5)+5, ",")
-    fmt.Print((rand.Float64() * 5) + 5)
-    fmt.Println()
+	// これを使って別の範囲の float 型の値を作ることもできる。
+	// 例えば`5.0 <= f' < 10.0` だと以下のようになる。
+	fmt.Print((rand.Float64()*5)+5, ",")
+	fmt.Print((rand.Float64() * 5) + 5)
+	fmt.Println()
 
-    // The default number generator is deterministic, so it'll
-    // produce the same sequence of numbers each time by default.
-    // To produce varying sequences, give it a seed that changes.
-    // Note that this is not safe to use for random numbers you
-    // intend to be secret, use `crypto/rand` for those.
-    s1 := rand.NewSource(time.Now().UnixNano())
-    r1 := rand.New(s1)
+	// デフォルトの生成機は決定的なので、プログラムを実行するたびに同じ数の列を生成する。
+	// 値を変えるためには、プログラムを実行するたびに変わるシードを渡せばよい。
+	// なお、math/rand の生成する乱数は、暗号用に使うべきではない。
+	// このような用途では、crypto/rand を使うべきだ。
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
 
-    // Call the resulting `rand.Rand` just like the
-    // functions on the `rand` package.
-    fmt.Print(r1.Intn(100), ",")
-    fmt.Print(r1.Intn(100))
-    fmt.Println()
+	// `r1` は `rand` パッケージの関数と同様のメソッドを持っている。
+	fmt.Print(r1.Intn(100), ",")
+	fmt.Print(r1.Intn(100))
+	fmt.Println()
 
-    // If you seed a source with the same number, it
-    // produces the same sequence of random numbers.
-    s2 := rand.NewSource(42)
-    r2 := rand.New(s2)
-    fmt.Print(r2.Intn(100), ",")
-    fmt.Print(r2.Intn(100))
-    fmt.Println()
-    s3 := rand.NewSource(42)
-    r3 := rand.New(s3)
-    fmt.Print(r3.Intn(100), ",")
-    fmt.Print(r3.Intn(100))
+	// 同じ数をシードにすると、同じ乱数列を生成する。
+	s2 := rand.NewSource(42)
+	r2 := rand.New(s2)
+	fmt.Print(r2.Intn(100), ",")
+	fmt.Print(r2.Intn(100))
+	fmt.Println()
+	s3 := rand.NewSource(42)
+	r3 := rand.New(s3)
+	fmt.Print(r3.Intn(100), ",")
+	fmt.Print(r3.Intn(100))
 }
