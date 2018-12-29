83f2e115ed622b8261f5fec86d322c48ac3a5832
spinute <spinutids@gmail.com>
2018-12-29 13:13:10 +0800

    translate mutexes

--- examples/mutexes/mutexes.go
+++ examples/mutexes/mutexes.go
@@ -1,85 +1,74 @@
-// In the previous example we saw how to manage simple
-// counter state using [atomic operations](atomic-counters).
-// For more complex state we can use a <em>[mutex](http://en.wikipedia.org/wiki/Mutual_exclusion)</em>
-// to safely access data across multiple goroutines.
+// 前の例で、単純なカウンタの状態を[アトミックな操作](atomic-counters)で管理する方法を紹介した。
+// より複雑な状態を管理したいときには、<em>[ミューテックス（mutex）](http://en.wikipedia.org/wiki/Mutual_exclusion)</em>を使って複数のゴルーチンから安全にデータを読み書きすることもできる。
 
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "sync/atomic"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
 func main() {
 
-    // For our example the `state` will be a map.
-    var state = make(map[int]int)
+	// 状態の例としてマップを使う。
+	var state = make(map[int]int)
 
-    // This `mutex` will synchronize access to `state`.
-    var mutex = &sync.Mutex{}
+	// この `mutex` を使って、`state` へのアクセスを同期する。
+	var mutex = &sync.Mutex{}
 
-    // We'll keep track of how many read and write
-    // operations we do.
-    var readOps uint64
-    var writeOps uint64
+	// 読み操作・書き操作がそれぞれ何度行われたかをカウントする。
+	var readOps uint64
+	var writeOps uint64
 
-    // Here we start 100 goroutines to execute repeated
-    // reads against the state, once per millisecond in
-    // each goroutine.
-    for r := 0; r < 100; r++ {
-        go func() {
-            total := 0
-            for {
+	// 100 個のゴルーチンを作り、それぞれが1ミリ秒ごとに state を繰り返し読み出す。
+	for r := 0; r < 100; r++ {
+		go func() {
+			total := 0
+			for {
 
-                // For each read we pick a key to access,
-                // `Lock()` the `mutex` to ensure
-                // exclusive access to the `state`, read
-                // the value at the chosen key,
-                // `Unlock()` the mutex, and increment
-                // the `readOps` count.
-                key := rand.Intn(5)
-                mutex.Lock()
-                total += state[key]
-                mutex.Unlock()
-                atomic.AddUint64(&readOps, 1)
+				// 読み出しのたびに、アクセスするキーを選ぶ。
+				// `Lock()` で `mutex` をロックし、`state` への排他的なアクセスを獲得し、値を読み出し、`Unlock()` で `mutex` のロックを解除し、最後に `readOps` の値を1増やす。
+				key := rand.Intn(5)
+				mutex.Lock()
+				total += state[key]
+				mutex.Unlock()
+				atomic.AddUint64(&readOps, 1)
 
-                // Wait a bit between reads.
-                time.Sleep(time.Millisecond)
-            }
-        }()
-    }
+				// 読み出しのたびに少し待つ。
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
 
-    // We'll also start 10 goroutines to simulate writes,
-    // using the same pattern we did for reads.
-    for w := 0; w < 10; w++ {
-        go func() {
-            for {
-                key := rand.Intn(5)
-                val := rand.Intn(100)
-                mutex.Lock()
-                state[key] = val
-                mutex.Unlock()
-                atomic.AddUint64(&writeOps, 1)
-                time.Sleep(time.Millisecond)
-            }
-        }()
-    }
+	// 10個のゴルーチンを起動し、読み出しと同様のパターンで書き込みを行う。
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
+				key := rand.Intn(5)
+				val := rand.Intn(100)
+				mutex.Lock()
+				state[key] = val
+				mutex.Unlock()
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
 
-    // Let the 10 goroutines work on the `state` and
-    // `mutex` for a second.
-    time.Sleep(time.Second)
+	// 10個のゴルイーチンが `state` や `mutex` で仕事をできるよう、一秒待つ。
+	time.Sleep(time.Second)
 
-    // Take and report final operation counts.
-    readOpsFinal := atomic.LoadUint64(&readOps)
-    fmt.Println("readOps:", readOpsFinal)
-    writeOpsFinal := atomic.LoadUint64(&writeOps)
-    fmt.Println("writeOps:", writeOpsFinal)
+	// 結果を読み出して、読み書き操作のカウントを報告する
+	readOpsFinal := atomic.LoadUint64(&readOps)
+	fmt.Println("readOps:", readOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps)
+	fmt.Println("writeOps:", writeOpsFinal)
 
-    // With a final lock of `state`, show how it ended up.
-    mutex.Lock()
-    fmt.Println("state:", state)
-    mutex.Unlock()
+	// `state` をロックして、最終的にどうなったかを表示する。
+	mutex.Lock()
+	fmt.Println("state:", state)
+	mutex.Unlock()
 }
