812c6ad72c5e5411c4b0b9d388091ae9a3ddc80e
spinute <spinutids@gmail.com>
2018-12-29 11:39:14 +0800

    translate if-else

--- examples/if-else/if-else.go
+++ examples/if-else/if-else.go
@@ -1,5 +1,4 @@
-// Branching with `if` and `else` in Go is
-// straight-forward.
+// Go における `if` と `else` を使った分岐の書き方は素直なものだ。
 
 package main
 
@@ -7,29 +6,27 @@ import "fmt"
 
 func main() {
 
-    // Here's a basic example.
-    if 7%2 == 0 {
-        fmt.Println("7 is even")
-    } else {
-        fmt.Println("7 is odd")
-    }
+	// 基本的な例
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
 
-    // You can have an `if` statement without an else.
-    if 8%4 == 0 {
-        fmt.Println("8 is divisible by 4")
-    }
+	// else がなくても `if` 文を書ける
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
 
-    // A statement can precede conditionals; any variables
-    // declared in this statement are available in all
-    // branches.
-    if num := 9; num < 0 {
-        fmt.Println(num, "is negative")
-    } else if num < 10 {
-        fmt.Println(num, "has 1 digit")
-    } else {
-        fmt.Println(num, "has multiple digits")
-    }
+	// 条件の前に文を書いてもよい。
+	// この文で宣言された変数は、いずれの分岐からも見える。
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
 }
 
-// Note that you don't need parentheses around conditions
-// in Go, but that the braces are required.
+// 注意点として、条件を囲む丸括弧は必要ないが、波括弧は必須である。
