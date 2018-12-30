15ba6247ab394745b295dec05a8e33afd384654e
spinute <spinutids@gmail.com>
2018-12-30 21:03:25 +1100

    translato select

--- examples/select/select.go
+++ examples/select/select.go
@@ -1,6 +1,5 @@
-// Go's _select_ lets you wait on multiple channel
-// operations. Combining goroutines and channels with
-// select is a powerful feature of Go.
+// Go の _select_ を使うと、複数のチャネルで受信を待てる。
+// ゴルーチンとチャネル、そして select の組み合わせは Go の強力な機能である。
 
 package main
 
@@ -9,30 +8,30 @@ import "fmt"
 
 func main() {
 
-    // For our example we'll select across two channels.
-    c1 := make(chan string)
-    c2 := make(chan string)
+	// この例では2つのチャネルに select を使う。
+	c1 := make(chan string)
+	c2 := make(chan string)
 
-    // Each channel will receive a value after some amount
-    // of time, to simulate e.g. blocking RPC operations
-    // executing in concurrent goroutines.
-    go func() {
-        time.Sleep(1 * time.Second)
-        c1 <- "one"
-    }()
-    go func() {
-        time.Sleep(2 * time.Second)
-        c2 <- "two"
-    }()
+	// いずれのチャネルも一定の時間が経てば値を受信する。
+	// これはゴルーチンが RPC を同期的に実行する状況を模している。
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
 
-    // We'll use `select` to await both of these values
-    // simultaneously, printing each one as it arrives.
-    for i := 0; i < 2; i++ {
-        select {
-        case msg1 := <-c1:
-            fmt.Println("received", msg1)
-        case msg2 := <-c2:
-            fmt.Println("received", msg2)
-        }
-    }
+	// We'll use `select` to await both of these values
+	// simultaneously, printing each one as it arrives.
+	// `select` を使ってふたつの値をいずれも非同期で受信し、届いた方から表示する。
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
 }
