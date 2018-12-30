ef4604501d0d22bd318b2e99eee2123d9db33364
spinute <spinutids@gmail.com>
2018-12-31 00:11:56 +1100

    translate sorting-by-functions

--- examples/sorting-by-functions/sorting-by-functions.go
+++ examples/sorting-by-functions/sorting-by-functions.go
@@ -1,43 +1,35 @@
-// Sometimes we'll want to sort a collection by something
-// other than its natural order. For example, suppose we
-// wanted to sort strings by their length instead of
-// alphabetically. Here's an example of custom sorts
-// in Go.
+// コレクションを特別な順序でソートしたいことがある。
+// 例えば、文字列を辞書順ではなく、その長さの順にソートしたいとする。
+// ここではそのようにソートをカスタムする方法を紹介する。
 
 package main
 
 import "sort"
 import "fmt"
 
-// In order to sort by a custom function in Go, we need a
-// corresponding type. Here we've created a `byLength`
-// type that is just an alias for the builtin `[]string`
-// type.
+// Go で独自の関数を使ってソートするには、そのための型を定義しなければならない。
+// ここでは `byLength` という型を定義するが、これは `[]string` の単なるエイリアスである。
 type byLength []string
 
-// We implement `sort.Interface` - `Len`, `Less`, and
-// `Swap` - on our type so we can use the `sort` package's
-// generic `Sort` function. `Len` and `Swap`
-// will usually be similar across types and `Less` will
-// hold the actual custom sorting logic. In our case we
-// want to sort in order of increasing string length, so
-// we use `len(s[i])` and `len(s[j])` here.
+// `sort.Interface` を実装する。
+// これは `Len`、`Less`、`Swap` の3つのメソッドを我々の型に実装するということだ。
+// こうすると、`sort` パッケージの汎用な `Sort` 関数を使えるようになる。
+// `Len` や `Swap` はどの型でも似たような実装になる。
+// `Less` こそが実際にソートのやり方を決める関数である。
+// 我々の場合、文字列の長さの昇順にしたいので、`len(s[i])` と `len(s[j])` を使って `Less` を実装する。
 func (s byLength) Len() int {
-    return len(s)
+	return len(s)
 }
 func (s byLength) Swap(i, j int) {
-    s[i], s[j] = s[j], s[i]
+	s[i], s[j] = s[j], s[i]
 }
 func (s byLength) Less(i, j int) bool {
-    return len(s[i]) < len(s[j])
+	return len(s[i]) < len(s[j])
 }
 
-// With all of this in place, we can now implement our
-// custom sort by casting the original `fruits` slice to
-// `byLength`, and then use `sort.Sort` on that typed
-// slice.
+// ここまでの準備をすれば、あとはスライス `fruits` を `byLength` 型にキャストし、`sort.Sort` を呼べば、独自のソートを実装できる。
 func main() {
-    fruits := []string{"peach", "banana", "kiwi"}
-    sort.Sort(byLength(fruits))
-    fmt.Println(fruits)
+	fruits := []string{"peach", "banana", "kiwi"}
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
 }
