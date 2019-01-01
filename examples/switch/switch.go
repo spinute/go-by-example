f746f3163b18c8634318aad0927ab617b7db169d
spinute <spinutids@gmail.com>
2019-01-01 21:20:15 +1100

    translate switch

--- examples/switch/switch.go
+++ examples/switch/switch.go
@@ -1,5 +1,4 @@
-// _Switch statements_ express conditionals across many
-// branches.
+// _switch 文_は多くの分岐からなる条件を表す。
 
 package main
 
@@ -8,54 +7,51 @@ import "time"
 
 func main() {
 
-    // Here's a basic `switch`.
-    i := 2
-    fmt.Print("Write ", i, " as ")
-    switch i {
-    case 1:
-        fmt.Println("one")
-    case 2:
-        fmt.Println("two")
-    case 3:
-        fmt.Println("three")
-    }
+	// `switch` の基本的な使い方
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
 
-    // You can use commas to separate multiple expressions
-    // in the same `case` statement. We use the optional
-    // `default` case in this example as well.
-    switch time.Now().Weekday() {
-    case time.Saturday, time.Sunday:
-        fmt.Println("It's the weekend")
-    default:
-        fmt.Println("It's a weekday")
-    }
+	// コンマを使うと一つの`case`文に複数の式を書ける。
+	// この例では`default`のケースも使っている。
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
 
-    // `switch` without an expression is an alternate way
-    // to express if/else logic. Here we also show how the
-    // `case` expressions can be non-constants.
-    t := time.Now()
-    switch {
-    case t.Hour() < 12:
-        fmt.Println("It's before noon")
-    default:
-        fmt.Println("It's after noon")
-    }
+	// `switch` の直後に式を書かなければ、それは単なる if/else の別の書き方である。
+	// また、定数でないものを使って `case` 式を書ける。
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
 
-    // A type `switch` compares types instead of values.  You
-    // can use this to discover the type of an interface
-    // value.  In this example, the variable `t` will have the
-    // type corresponding to its clause.
-    whatAmI := func(i interface{}) {
-        switch t := i.(type) {
-        case bool:
-            fmt.Println("I'm a bool")
-        case int:
-            fmt.Println("I'm an int")
-        default:
-            fmt.Printf("Don't know type %T\n", t)
-        }
-    }
-    whatAmI(true)
-    whatAmI(1)
-    whatAmI("hey")
+	// `switch` は値だけでなく型について分岐することもできる。
+	// これを使って、インターフェースの型を調べることができる。
+	// この例では、変数 `t` は対応する節に書かれた型を持つ。
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
 }
