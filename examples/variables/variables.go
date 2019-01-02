5835d3ae4a83e3a5cab296a6bc1e8aa843b41f2b
spinute <spinutids@gmail.com>
2019-01-02 19:11:35 +1100

    translate variables

--- examples/variables/variables.go
+++ examples/variables/variables.go
@@ -1,6 +1,4 @@
-// In Go, _variables_ are explicitly declared and used by
-// the compiler to e.g. check type-correctness of function
-// calls.
+// Go の_変数_は明示的に宣言され、コンパイラによってチェックされる（例えば、関数呼び出しの型安全性など）。
 
 package main
 
@@ -8,27 +6,25 @@ import "fmt"
 
 func main() {
 
-    // `var` declares 1 or more variables.
-    var a = "initial"
-    fmt.Println(a)
+	// `var` を使って、1つまたはそれ以上の変数を宣言できる。
+	var a = "initial"
+	fmt.Println(a)
 
-    // You can declare multiple variables at once.
-    var b, c int = 1, 2
-    fmt.Println(b, c)
+	// 複数の変数を一度に宣言することもできる。
+	var b, c int = 1, 2
+	fmt.Println(b, c)
 
-    // Go will infer the type of initialized variables.
-    var d = true
-    fmt.Println(d)
+	// 変数を初期化すれば Go は変数の型を推測する。
+	var d = true
+	fmt.Println(d)
 
-    // Variables declared without a corresponding
-    // initialization are _zero-valued_. For example, the
-    // zero value for an `int` is `0`.
-    var e int
-    fmt.Println(e)
+	// 明示的に初期化していない変数の値は_ゼロ値_である。
+	// 例えば、`int` のゼロ値は `0` である。
+	var e int
+	fmt.Println(e)
 
-    // The `:=` syntax is shorthand for declaring and
-    // initializing a variable, e.g. for
-    // `var f string = "short"` in this case.
-    f := "short"
-    fmt.Println(f)
+	// `:=` は宣言と初期化を行うための省略記法である。
+	// ここの例は `var f string = "short"` と書くのと同じ意味である。
+	f := "short"
+	fmt.Println(f)
 }
