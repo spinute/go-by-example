878d868af5e325cfad49df9f37ddeb48e20f6449
spinute <spinutids@gmail.com>
2019-01-02 19:35:57 +1100

    translate writing-files

--- examples/writing-files/writing-files.go
+++ examples/writing-files/writing-files.go
@@ -1,58 +1,54 @@
-// Writing files in Go follows similar patterns to the
-// ones we saw earlier for reading.
+// Go でファイルに書き込むには、前に見たファイルを読み出すのと似たパターンを使う。
 
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io/ioutil"
-    "os"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
 
-    // To start, here's how to dump a string (or just
-    // bytes) into a file.
-    d1 := []byte("hello\ngo\n")
-    err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
-    check(err)
+	// まずは文字列（またはバイト列）を単にファイルに書き込む。
+	d1 := []byte("hello\ngo\n")
+	err := ioutil.WriteFile("/tmp/dat1", d1, 0644)
+	check(err)
 
-    // For more granular writes, open a file for writing.
-    f, err := os.Create("/tmp/dat2")
-    check(err)
+	// より細かな制御をするために、ファイルを書き込み用に開く。
+	f, err := os.Create("/tmp/dat2")
+	check(err)
 
-    // It's idiomatic to defer a `Close` immediately
-    // after opening a file.
-    defer f.Close()
+	// ファイルを開いた直後に `Close` を `defer` するのはイディオムである。
+	defer f.Close()
 
-    // You can `Write` byte slices as you'd expect.
-    d2 := []byte{115, 111, 109, 101, 10}
-    n2, err := f.Write(d2)
-    check(err)
-    fmt.Printf("wrote %d bytes\n", n2)
+	// `Write` を使ってバイトのスライスを書き込める。
+	d2 := []byte{115, 111, 109, 101, 10}
+	n2, err := f.Write(d2)
+	check(err)
+	fmt.Printf("wrote %d bytes\n", n2)
 
-    // A `WriteString` is also available.
-    n3, err := f.WriteString("writes\n")
-    fmt.Printf("wrote %d bytes\n", n3)
+	// `WriteString` という関数もあり、こちらは文字列を書き込める。
+	n3, err := f.WriteString("writes\n")
+	fmt.Printf("wrote %d bytes\n", n3)
 
-    // Issue a `Sync` to flush writes to stable storage.
-    f.Sync()
+	// `Sync` を使って書き込みをストレージにフラッシュする。
+	f.Sync()
 
-    // `bufio` provides buffered writers in addition
-    // to the buffered readers we saw earlier.
-    w := bufio.NewWriter(f)
-    n4, err := w.WriteString("buffered\n")
-    fmt.Printf("wrote %d bytes\n", n4)
+	// `bufio` を使ってバッファ付きのリーダーを作る例を前に紹介した。
+	// 同様に、バッファ付きのライターも作れる。
+	w := bufio.NewWriter(f)
+	n4, err := w.WriteString("buffered\n")
+	fmt.Printf("wrote %d bytes\n", n4)
 
-    // Use `Flush` to ensure all buffered operations have
-    // been applied to the underlying writer.
-    w.Flush()
+	// `Flush` を使って、ライターのバッファされている操作をすべてフラッシュする。
+	w.Flush()
 
 }
