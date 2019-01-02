1ebefa6ad745de37b69aea2c1f5e53a7c7b112af
spinute <spinutids@gmail.com>
2019-01-02 17:03:15 +1100

    translate time-formatting-parsing

--- examples/time-formatting-parsing/time-formatting-parsing.go
+++ examples/time-formatting-parsing/time-formatting-parsing.go
@@ -1,5 +1,4 @@
-// Go supports time formatting and parsing via
-// pattern-based layouts.
+// Go ではパターンに基づくレイアウトを利用して時刻をフォーマットできる。
 
 package main
 
@@ -7,44 +6,36 @@ import "fmt"
 import "time"
 
 func main() {
-    p := fmt.Println
+	p := fmt.Println
 
-    // Here's a basic example of formatting a time
-    // according to RFC3339, using the corresponding layout
-    // constant.
-    t := time.Now()
-    p(t.Format(time.RFC3339))
+	// まずは RFC3339 に対応するレイアウトを使って時刻をフォーマットする方法を紹介する。
+	t := time.Now()
+	p(t.Format(time.RFC3339))
 
-    // Time parsing uses the same layout values as `Format`.
-    t1, e := time.Parse(
-        time.RFC3339,
-        "2012-11-01T22:08:41+00:00")
-    p(t1)
+	// 時刻をパースするときも `Format` と同じレイアウトを使う。
+	t1, e := time.Parse(
+		time.RFC3339,
+		"2012-11-01T22:08:41+00:00")
+	p(t1)
 
-    // `Format` and `Parse` use example-based layouts. Usually
-    // you'll use a constant from `time` for these layouts, but
-    // you can also supply custom layouts. Layouts must use the
-    // reference time `Mon Jan 2 15:04:05 MST 2006` to show the
-    // pattern with which to format/parse a given time/string.
-    // The example time must be exactly as shown: the year 2006,
-    // 15 for the hour, Monday for the day of the week, etc.
-    p(t.Format("3:04PM"))
-    p(t.Format("Mon Jan _2 15:04:05 2006"))
-    p(t.Format("2006-01-02T15:04:05.999999-07:00"))
-    form := "3 04 PM"
-    t2, e := time.Parse(form, "8 41 PM")
-    p(t2)
+	// `Format`、`Parse` では例示に基づいてレイアウトを決める。
+	// ふつうは `time` モジュールに定義されている定数をレイアウトの例として使うが、独自のレイアウトを使ってもよい。
+	// レイアウトでは特定の時刻 `Mon Jan 2 15:04:05 MST 2006` を表している必要があり、プログラムはこれに従って時刻をフォーマットしたり、文字列をパースしたりする。
+	// 時刻の例はちょうど以下のようなものだ。例えば、以下の例では、年は2006に、時間は15に、曜日は月曜になっている。
+	p(t.Format("3:04PM"))
+	p(t.Format("Mon Jan _2 15:04:05 2006"))
+	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+	form := "3 04 PM"
+	t2, e := time.Parse(form, "8 41 PM")
+	p(t2)
 
-    // For purely numeric representations you can also
-    // use standard string formatting with the extracted
-    // components of the time value.
-    fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
-        t.Year(), t.Month(), t.Day(),
-        t.Hour(), t.Minute(), t.Second())
+	// フォーマットしたいのが数値なら、時刻値の部分を抜き出し、文字列のフォーマット機能を使ってもよい。
+	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+		t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
 
-    // `Parse` will return an error on malformed input
-    // explaining the parsing problem.
-    ansic := "Mon Jan _2 15:04:05 2006"
-    _, e = time.Parse(ansic, "8:41PM")
-    p(e)
+	// `Parse` は入力が不正だと、パース時に起きた問題を説明するエラーを返す。
+	ansic := "Mon Jan _2 15:04:05 2006"
+	_, e = time.Parse(ansic, "8:41PM")
+	p(e)
 }
