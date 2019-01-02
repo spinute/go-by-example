5841116e9ec36649efcb5a0f73c1510b93d0d04e
spinute <spinutids@gmail.com>
2019-01-02 17:16:53 +1100

    translate time

--- examples/time/time.go
+++ examples/time/time.go
@@ -1,5 +1,5 @@
-// Go offers extensive support for times and durations;
-// here are some examples.
+// Go には時刻や経過時間を扱うための機能が多くある。
+// ここではその例を示す。
 
 package main
 
@@ -7,55 +7,47 @@ import "fmt"
 import "time"
 
 func main() {
-    p := fmt.Println
+	p := fmt.Println
 
-    // We'll start by getting the current time.
-    now := time.Now()
-    p(now)
+	// まずは現在時刻を取得する。
+	now := time.Now()
+	p(now)
 
-    // You can build a `time` struct by providing the
-    // year, month, day, etc. Times are always associated
-    // with a `Location`, i.e. time zone.
-    then := time.Date(
-        2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
-    p(then)
+	// `time` 構造体を、年・月・日などを指定して作ることもできる。
+	// 時刻は常に位置（`Location`）、すなわちタイムゾーンと結びついている。
+	then := time.Date(
+		2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+	p(then)
 
-    // You can extract the various components of the time
-    // value as expected.
-    p(then.Year())
-    p(then.Month())
-    p(then.Day())
-    p(then.Hour())
-    p(then.Minute())
-    p(then.Second())
-    p(then.Nanosecond())
-    p(then.Location())
+	// 時刻の構成要素の取り出しかたは直感的だ。
+	p(then.Year())
+	p(then.Month())
+	p(then.Day())
+	p(then.Hour())
+	p(then.Minute())
+	p(then.Second())
+	p(then.Nanosecond())
+	p(then.Location())
 
-    // The Monday-Sunday `Weekday` is also available.
-    p(then.Weekday())
+	// 曜日（Monday-Sunday）を表す `Weekday` もある。
+	p(then.Weekday())
 
-    // These methods compare two times, testing if the
-    // first occurs before, after, or at the same time
-    // as the second, respectively.
-    p(then.Before(now))
-    p(then.After(now))
-    p(then.Equal(now))
+	// 以下のメソッドは2つの時刻を比べて、1つ目が2つ目より前か、後か、それとも同時刻かをそれぞれテストする。
+	p(then.Before(now))
+	p(then.After(now))
+	p(then.Equal(now))
 
-    // The `Sub` methods returns a `Duration` representing
-    // the interval between two times.
-    diff := now.Sub(then)
-    p(diff)
+	// メソッド `Sub` は2つの時刻の間隔を表す経過時間 `Duration` を返す。
+	diff := now.Sub(then)
+	p(diff)
 
-    // We can compute the length of the duration in
-    // various units.
-    p(diff.Hours())
-    p(diff.Minutes())
-    p(diff.Seconds())
-    p(diff.Nanoseconds())
+	// 経過時間の長さの単位を変換できる。
+	p(diff.Hours())
+	p(diff.Minutes())
+	p(diff.Seconds())
+	p(diff.Nanoseconds())
 
-    // You can use `Add` to advance a time by a given
-    // duration, or with a `-` to move backwards by a
-    // duration.
-    p(then.Add(diff))
-    p(then.Add(-diff))
+	// `Add` を使って指定した期間だけ時刻を進めることもできる。`-` を使えば、時刻を戻すこともできる。
+	p(then.Add(diff))
+	p(then.Add(-diff))
 }
