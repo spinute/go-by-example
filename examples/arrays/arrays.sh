5d212a9ae055d7ff447f362c2a249e084b9ca939
spinute <spinutids@gmail.com>
2019-01-02 20:00:22 +1100

    transllate arrays.sh

--- examples/arrays/arrays.sh
+++ examples/arrays/arrays.sh
@@ -1,5 +1,4 @@
-# Note that arrays appear in the form `[v1 v2 v3 ...]`
-# when printed with `fmt.Println`.
+# `fmt.Println` に配列を渡すと `[v1 v2 v3 ...]` と表示される。
 $ go run arrays.go
 emp: [0 0 0 0 0]
 set: [0 0 0 0 100]
@@ -8,5 +7,5 @@ len: 5
 dcl: [1 2 3 4 5]
 2d:  [[0 1 2] [1 2 3]]
 
-# You'll see _slices_ much more often than arrays in
-# typical Go. We'll look at slices next.
+# Go では普通、配列より_スライス_をよく見るだろう。
+# スライスについては、次の例で見ていく。
