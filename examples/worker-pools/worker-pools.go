c819ee0ee4bb9d3cd3040c2b9fd1391cd5943338
spinute <spinutids@gmail.com>
2019-01-02 19:25:33 +1100

    translate worker-pools

--- examples/worker-pools/worker-pools.go
+++ examples/worker-pools/worker-pools.go
@@ -1,48 +1,43 @@
-// In this example we'll look at how to implement
-// a _worker pool_ using goroutines and channels.
+// この例ではゴルーチンとチャネルを使って_ワーカープール_を実装するやり方を紹介する。
 
 package main
 
 import "fmt"
 import "time"
 
-// Here's the worker, of which we'll run several
-// concurrent instances. These workers will receive
-// work on the `jobs` channel and send the corresponding
-// results on `results`. We'll sleep a second per job to
-// simulate an expensive task.
+// このワーカーをいくつか平行に動かす。
+// 各ワーカーは `jobs` チャネルに仕事を受信し、その結果を `results` チャネルに送信する。
+// 実行に時間のかかる処理をシミュレートするために、仕事をするたびに1秒スリープする。
 func worker(id int, jobs <-chan int, results chan<- int) {
-    for j := range jobs {
-        fmt.Println("worker", id, "started  job", j)
-        time.Sleep(time.Second)
-        fmt.Println("worker", id, "finished job", j)
-        results <- j * 2
-    }
+	for j := range jobs {
+		fmt.Println("worker", id, "started  job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
 }
 
 func main() {
 
-    // In order to use our pool of workers we need to send
-    // them work and collect their results. We make 2
-    // channels for this.
-    jobs := make(chan int, 100)
-    results := make(chan int, 100)
+	// ワーカープールを使うために、仕事を送り、結果を収集する必要がある。
+	// そのために使う2つのチャネルを作る。
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
 
-    // This starts up 3 workers, initially blocked
-    // because there are no jobs yet.
-    for w := 1; w <= 3; w++ {
-        go worker(w, jobs, results)
-    }
+	// ここで3つワーカーを起動する。
+	// まだジョブがないので、いずれのワーカーもブロックする。
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
 
-    // Here we send 5 `jobs` and then `close` that
-    // channel to indicate that's all the work we have.
-    for j := 1; j <= 5; j++ {
-        jobs <- j
-    }
-    close(jobs)
+	// `jobs` に値を5つ送り、その後チャネルを閉じて仕事を送り終えたことを伝える。
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
 
-    // Finally we collect all the results of the work.
-    for a := 1; a <= 5; a++ {
-        <-results
-    }
+	// 最後に仕事の結果を集める。
+	for a := 1; a <= 5; a++ {
+		<-results
+	}
 }
